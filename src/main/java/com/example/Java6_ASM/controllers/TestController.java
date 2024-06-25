88f69bfc3ccf1f6618568d054ae114ea1558d81d
tridung778 <crytv778@gmail.com>
2024-06-25 14:58:31 +0700

    d

--- src/main/java/com/example/Java6_ASM/controllers/TestController.java
+++ src/main/java/com/example/Java6_ASM/controllers/TestController.java
@@ -10,7 +10,7 @@ public class TestController {
 
     @RequestMapping("")
     public String index(Model model) {
-        model.addAttribute("message", "Luong!");
+        model.addAttribute("message", "Dung!");
         return "index";
     }
 }
