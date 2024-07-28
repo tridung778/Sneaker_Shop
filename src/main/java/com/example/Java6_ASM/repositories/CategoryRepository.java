ffb832ef19c8ac92dc977fc452d94aa9494ada33
Dương Trí Dũng <crytv778@gmail.com>
2024-07-28 22:21:10 +0700

    add product from products.json to database

--- src/main/java/com/example/Java6_ASM/repositories/CategoryRepository.java
+++ src/main/java/com/example/Java6_ASM/repositories/CategoryRepository.java
@@ -3,7 +3,9 @@ package com.example.Java6_ASM.repositories;
 import com.example.Java6_ASM.models.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface CategoryRepository extends JpaRepository<Category, UUID> {
+    Optional<Category> findByName(String categoryName);
 }
--- src/main/java/com/example/Java6_ASM/services/ProductService.java
+++ src/main/java/com/example/Java6_ASM/services/ProductService.java
@@ -1,4 +1,9 @@
 package com.example.Java6_ASM.services;
 
+import com.example.Java6_ASM.models.Product;
+
+import java.util.List;
+
 public interface ProductService {
+    void saveAllProduct(List<Product> products);
 }
