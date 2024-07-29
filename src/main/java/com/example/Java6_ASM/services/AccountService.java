7e252b05c812267ec62100412f27f1a2204ee449
Thanhanz2 <thanhannguyen415@gmail.com>
2024-07-30 00:15:40 +0700

    login

--- src/main/java/com/example/Java6_ASM/services/AccountService.java
+++ src/main/java/com/example/Java6_ASM/services/AccountService.java
@@ -1,4 +1,11 @@
 package com.example.Java6_ASM.services;
 
+import java.util.List;
+
+import com.example.Java6_ASM.models.Account;
+
 public interface AccountService {
+	Account findById(String username);
+
+	List<Account> findAll();
 }
--- src/main/java/com/example/Java6_ASM/services/implement/AccountServiceImpl.java
+++ src/main/java/com/example/Java6_ASM/services/implement/AccountServiceImpl.java
@@ -1,8 +1,25 @@
 package com.example.Java6_ASM.services.implement;
 
+import com.example.Java6_ASM.models.Account;
+import com.example.Java6_ASM.repositories.AccountRepository;
 import com.example.Java6_ASM.services.AccountService;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AccountServiceImpl implements AccountService {
+	@Autowired
+	AccountRepository dao;
+
+	@Override
+	public Account findById(String username) {
+		return dao.findById(username);
+	}
+
+	public List<Account> findAll() {
+		return dao.findAll();
+	}
 }
